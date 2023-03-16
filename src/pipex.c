91814b26afc836bd508b2ad30dd14da5fc6c5b77
yiwong <yiwong@student.42wolfsburg.de>
2023-03-16 15:37:39 +0100

    save

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 19:11:52 by yiwong            #+#    #+#             */
-/*   Updated: 2023/03/13 19:25:46 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/03/16 14:39:52 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 int	main(int argc, char *argv[])
 {
-	parse_command(argc, argv);
+	ready(argc, argv);
 }
--- src/ready.c
+++ src/ready.c
@@ -6,10 +6,28 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 19:25:57 by yiwong            #+#    #+#             */
-/*   Updated: 2023/03/13 19:26:14 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/03/16 15:26:50 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../lib/pipex.h"
- 
- 
+
+int	ready(int argc, char *argv[])
+{
+	//open in + out
+	int fd_in;
+
+	fd_in = open(argv[i], O_RDONLY);
+	if (fd_in == -1)
+		perror();
+	//send commands to 
+}
+
+int	parse_command(int argc, char *argv[])
+{
+	//env search
+	//strncmp 5
+	//split
+	//cat cmd
+	//
+}
