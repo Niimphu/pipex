52a2467bae9a9f49b92f39d4baaa6158280f28f6
yiwong <yiwong@student.42wolfsburg.de>
2023-03-17 17:17:57 +0100

    update

--- src/main.c
+++ src/main.c
@@ -6,22 +6,16 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 19:25:57 by yiwong            #+#    #+#             */
-/*   Updated: 2023/03/16 18:28:57 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/03/17 17:16:00 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../lib/pipex.h"
 
-int	ready(int argc, char *argv[], char *envp[])
+int	initialise(int argc, char *argv[], char *envp[])
 {
 	//open in + out
-	int fd_in;
-	int	ft_out;
-
-	fd_in = open(argv[1], O_RDONLY);
-	fd_out = open(argv[argc - 1], O_RDWR);
-	if (fd_in == -1 || fd_out == -1)
-		perror();
+	
 	
 	//send commands to 
 }
--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 19:11:52 by yiwong            #+#    #+#             */
-/*   Updated: 2023/03/16 18:28:21 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/03/17 17:16:19 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 
 int	main(int argc, char *argv[], char *envp[])
 {
-	ready(argc, argv, envp);
+	int fd_in;
+	int	ft_out;
+
+	if (argc < 5)
+		return (ft_printf("Not enough arguments."));
+	fd_in = open(argv[1], O_RDONLY);
+	fd_out = open(argv[argc - 1], O_RDWR);
+	if (fd_in == -1 || fd_out == -1)
+		perror();
 }
