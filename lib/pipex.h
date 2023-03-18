6a2b7eb63913c64ed17d99189559310f2fc00a49
yiwong <yiwong@student.42wolfsburg.de>
2023-03-18 18:46:14 +0100

    save

--- lib/pipex.h
+++ lib/pipex.h
@@ -6,7 +6,7 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 19:11:58 by yiwong            #+#    #+#             */
-/*   Updated: 2023/03/13 19:14:28 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/03/18 18:32:52 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,12 @@
 # include <stdio.h>
 # include <unistd.h>
 
+typedef struct s_cmds
+{
+	int		*fd[2];
+	char	*cmd;
+	char	**path;
+	char	*args[512];
+}			t_cmds;
 
 #endif
--- src/pipex.c
+++ src/pipex.c
@@ -6,24 +6,26 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 19:11:52 by yiwong            #+#    #+#             */
-/*   Updated: 2023/03/17 17:21:21 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/03/18 18:37:15 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../lib/pipex.h"
 
-int	main(int argc, char *argv[], char *envp[])
+int	pipex(char *argv[], char *envp[], int *fd[])
 {
-	int *fd[2];
+	t_cmds	
+}
 
-	if (argc < 5)
-		return (ft_printf("Not enough arguments."));
-	fd[0] = open(argv[1], O_RDONLY);
-	fd[1] = open(argv[argc - 1], O_RDWR);
-	if (fd_in == -1 || fd_out == -1)
-		return (perror());
-	pipex(argv, envp, fd);
-	if (close(fd[0]) == -1 || close(fd[1]) == -1)
-		return (perror());
-	return (0);
+int	parse_command(char *argv[], char *envp[], int *fd[])
+{
+	int	i;
+
+	i = 0;
+	while ()
+	//env search
+	//strncmp 5
+	//split
+	//cat cmd
+	//
 }
