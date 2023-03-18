bbd365f239e44b5c92b740d6cf4faa57ed471a55
yiwong <yiwong@student.42wolfsburg.de>
2023-03-18 18:36:23 +0100

    maybeFinal

--- src/main.c
+++ src/main.c
@@ -6,26 +6,24 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 19:25:57 by yiwong            #+#    #+#             */
-/*   Updated: 2023/03/17 17:28:51 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/03/18 16:46:47 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../lib/pipex.h"
 
-int	pipex(char *argv[], char *envp[], int *fd[])
+int	main(int argc, char *argv[], char *envp[])
 {
-	
-}
+	int *fd[2];
 
-int	parse_command(char *argv[], char *envp[], int *fd[])
-{
-	int	i;
-
-	i = 0;
-	while ()
-	//env search
-	//strncmp 5
-	//split
-	//cat cmd
-	//
+	if (argc < 5)
+		return (ft_printf("Not enough arguments."));
+	fd[0] = open(argv[1], O_RDONLY);
+	fd[1] = open(argv[argc - 1], O_RDWR);
+	if (fd_in == -1 || fd_out == -1)
+		return (perror());
+	pipex(argv, envp, fd);
+	if (close(fd[0]) == -1 || close(fd[1]) == -1)
+		return (perror());
+	return (0);
 }
