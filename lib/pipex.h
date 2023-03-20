5bf16a405ae5c89812ec4039363692764eaaef8a
yiwong <yiwong@student.42wolfsburg.de>
2023-03-20 02:49:30 +0100

    save

--- lib/pipex.h
+++ lib/pipex.h
@@ -6,7 +6,7 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 19:11:58 by yiwong            #+#    #+#             */
-/*   Updated: 2023/03/18 18:32:52 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/03/19 19:02:22 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 
 typedef struct s_cmds
 {
-	int		*fd[2];
+	int		fd[2];
 	char	*cmd;
 	char	**path;
 	char	*args[512];
--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 19:11:52 by yiwong            #+#    #+#             */
-/*   Updated: 2023/03/18 18:37:15 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/03/20 02:42:43 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,21 @@
 
 int	pipex(char *argv[], char *envp[], int *fd[])
 {
-	t_cmds	
+	t_cmds data;
+	
+	data = struct_init(argv, envp, fd);
+	
+}
+
+t_cmds *struct_init(char *argv[], char *envp[], int *fd_in[])
+{
+	t_cmds	data;
+
+	data = calloc(sizeof(s_cmds*), 1);
+	if (!data)
+		return (NULL);
+	data -> fd = *fd_in;
+	
 }
 
 int	parse_command(char *argv[], char *envp[], int *fd[])
