06c37ba650e66c36c56d7f0568f16248a519fe79
yiwong <yiwong@student.42wolfsburg.de>
2023-03-21 04:23:26 +0100

    init

---
+++ src/free_memory.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_memory.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/21 03:45:38 by yiwong            #+#    #+#             */
+/*   Updated: 2023/03/21 03:45:38 by yiwong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../lib/pipex.h"
+
+void	free_cmds(t_cmds *data)
+{
+	if (data -> fd)
+	{
+		free(fd[0]);
+		fd[0] = NULL;
+		free(fd[1]);
+		fd[1] = NULL;
+	}
+	if (data -> cmd)
+		free_pointer(data -> cmd);
+	if (data -> path)
+		free_ppointer(data -> path);
+	if (data -> args)
+		free_ppointer(data -> args);
+	return ;
+}
+
+void	free_pointer(char *str)
+{
+	if (str)
+	{
+		free (str);
+		str = NULL;
+	}
+	return ;
+}
+
+void	free_ppointer(char **str)
+{
+	i = 0;
+	if (**str)
+	{
+		while (str[i])
+			free_pointer(str[i++]);
+		free(str);
+		str = NULL;
+	}
+	return ;
+}
