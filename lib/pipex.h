fc4cd6ea77151b586d54527cb01ef9de7cf5de86
yiwong <yiwong@student.42wolfsburg.de>
2023-03-21 19:06:41 +0100

    norm

--- lib/pipex.h
+++ lib/pipex.h
@@ -6,7 +6,7 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 19:11:58 by yiwong            #+#    #+#             */
-/*   Updated: 2023/03/21 19:03:50 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/03/21 19:06:32 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,6 @@ int		pipe_fork(char *cmd, t_cmds *data);
 int		execute(t_cmds *data);
 char	*find_exec(char *cmd, char **paths);
 char	*create_path(char *cmd, char *path);
-
 
 void	print_array(char **arr);
 
