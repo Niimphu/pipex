a3040f4b18eaeadbb2410731bd8513e3106124ae
yiwong <yiwong@student.42wolfsburg.de>
2023-03-22 22:23:38 +0100

    save

--- lib/pipex.h
+++ lib/pipex.h
@@ -6,7 +6,7 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 19:11:58 by yiwong            #+#    #+#             */
-/*   Updated: 2023/03/22 00:06:53 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/03/22 22:21:14 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <sys/wait.h>
+# include <errno.h>
 # include "libft/libft.h"
 # include "ft_printf/ft_printf.h"
 
@@ -33,16 +34,19 @@ typedef struct s_cmds
 int		pipex(char *argv[], char *envp[], int fd[]);
 t_cmds	*struct_init(char *envp[], int fd_in[]);
 char	**find_paths(char *envp[]);
-char	*path_trim(char *str);
+
+int		fork_this(char *cmd, t_cmds *data, int pipe_fd[]);
+int		execute(t_cmds *data);
+char	*find_exec(char *cmd, char **paths);
+char	*create_path(char *cmd, char *path);
 
 void	free_cmds(t_cmds *data);
 void	free_pointer(char *str);
 void	free_ppointer(char **str);
 
-int		fork_pipe(char *cmd, t_cmds *data);
-int		execute(t_cmds *data);
-char	*find_exec(char *cmd, char **paths);
-char	*create_path(char *cmd, char *path);
+void	error_exit(t_cmds *data, char *strerr);
+
+char	*path_trim(char *str);
 
 void	print_array(char **arr);
 
