efb4ef6d9e699e53e2264e09bf0806baa71ebef7
yiwong <yiwong@student.42wolfsburg.de>
2023-03-24 00:23:11 +0100

    edit

--- src/free_memory.c
+++ src/free_memory.c
@@ -11,39 +11,3 @@
 /* ************************************************************************** */
 
 #include "../lib/pipex.h"
-
-void	free_cmds(t_cmds *data)
-{
-	if (data -> cmd)
-		free_pointer(data -> cmd);
-	if (data -> path)
-		free_ppointer(data -> path);
-	if (data -> args)
-		free_ppointer(data -> args);
-	return ;
-}
-
-void	free_pointer(char *str)
-{
-	if (str)
-	{
-		free(str);
-		str = NULL;
-	}
-	return ;
-}
-
-void	free_ppointer(char **str)
-{
-	int	i;
-
-	i = 0;
-	if (str)
-	{
-		while (str[i])
-			free_pointer(str[i++]);
-		free(str);
-		str = NULL;
-	}
-	return ;
-}
